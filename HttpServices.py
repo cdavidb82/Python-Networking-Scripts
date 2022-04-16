ad05f6ca2684a9bf6fcc5a76b3da225783579190
David Barnett <cdavidb82@outlook.com>
2022-04-16 11:36:00 -0400

    Initial Commit

---
+++ HttpServices.py
@@ -0,0 +1,16 @@
+import urllib
+import urllib.request
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+goog_url = 'http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv'
+
+
+def download(csv_url):
+    response = urllib.request.urlopen(csv_url)
+    csv = response.read()
+    csv_str = str(csv)
+    lines = csv_str.split("\\n")
+    for l in lines:
+        pp.pprint(l + "\n")
