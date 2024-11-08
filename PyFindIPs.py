b3e5e10dc5214aea934e95c76ca4ee6cac0db26b
David <cdavidb82@outlook.com>
2024-11-07 21:37:32 -0500

    Added docstrings to functions and conditional check for adapters in main function.

--- PyFindIPs.py
+++ PyFindIPs.py
@@ -1,12 +1,13 @@
 # IP Scanner
-
 import ifaddr
 import pprint
 
 def get_network_adapters():
+    """Retrieve a list of network adapters."""
     return ifaddr.get_adapters()
 
 def print_adapter_ips(adapters):
+    """Print the IP addresses of the given network adapters."""
     pp = pprint.PrettyPrinter(indent=1)
     for adapter in adapters:
         pp.pprint(f'IPs of network adapter {adapter.nice_name}')
@@ -14,8 +15,12 @@ def print_adapter_ips(adapters):
             print(f'  {ip.ip}/{ip.network_prefix}')
 
 def main():
+    """Main entry point of the program."""
     adapters = get_network_adapters()
-    print_adapter_ips(adapters)
+    if adapters:
+        print_adapter_ips(adapters)
+    else:
+        print("No network adapters found.")
 
 if __name__ == '__main__':
     main()
